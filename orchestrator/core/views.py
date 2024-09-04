6c3d7838f8ad0ba8ef1d112a86a12f0531975142
WillianM19 <willianm1928@gmail.com>
2024-09-03 21:39:27 -0300

    feat: finalizando orquestrador

--- orchestrator/core/views.py
+++ orchestrator/core/views.py
@@ -9,14 +9,14 @@ class PurchaseProductView(views.APIView):
         quantity = request.data.get('quantity')
 
         try:
-            inventory_check_response = requests.get(f'http://localhost:8002/api/inventory/{product_id}/')
-            inventory_check_response.raise_for_status()
-
-            inventory_data = inventory_check_response.json()
-            if inventory_data['quantity'] < quantity:
-                return Response({'error': 'Product not available in inventory'}, status=400)
+            response  = requests.get(f'http://localhost:8002/api/inventory/{product_id}')
+            data = response.json()
+            
+            if data.get('stock', 0) < quantity:
+                return Response({'error': f"Não há estoque suficiente para este pedido."}, status=400)
+        
         except requests.RequestException:
-            return Response({'error': 'Failed to check inventory'}, status=500)
+            return Response({'error': 'Failed to create order'}, status=500)
 
         try:
             order_response = requests.post('http://localhost:8001/api/orders/', json={
@@ -25,41 +25,37 @@ class PurchaseProductView(views.APIView):
             })
             order_response.raise_for_status()
 
-            if order_response.status_code != 201:
+            if order_response.status_code != 200:
                 return Response({'error': 'Failed to create order'}, status=400)
 
-            order_id = order_response.json().get('order_id')
         except requests.RequestException:
             return Response({'error': 'Failed to create order'}, status=500)
 
+        order_id = order_response.json().get('order_id')
+
         try:
-            inventory_reserve_response = requests.post('http://localhost:8002/api/inventory/', json={
+            inventory_reserve_response = requests.post('http://localhost:8002/api/inventory/reserve/', json={
                 'product_id': product_id,
                 'quantity': quantity
             })
             inventory_reserve_response.raise_for_status()
 
-            if inventory_reserve_response.status_code != 201:
+            if inventory_reserve_response.status_code != 200:
                 return Response({'error': 'Failed to reserve inventory'}, status=400)
+
         except requests.RequestException:
             return Response({'error': 'Failed to reserve inventory'}, status=500)
 
         try:
-            payment_response = requests.post('http://localhost:8003/api/payments/', json={
+            response = requests.post(f'http://localhost:8003/api/payment/', json={
                 'order_id': order_id,
-                'value': quantity * 10
+                'value': data.get('product', 0)['price']
             })
-            payment_response.raise_for_status()
 
-            if payment_response.status_code != 200:
-                requests.put(f'http://localhost:8002/api/inventory/{product_id}/', json={
-                    'quantity': quantity
-                })
-                return Response({'error': 'Failed to process payment'}, status=400)
+            if response.status_code != 200:
+                return Response({'error': 'Failed to reserve inventory'}, status=400)
+
         except requests.RequestException:
-            requests.put(f'http://localhost:8002/api/inventory/{product_id}/', json={
-                'quantity': quantity
-            })
-            return Response({'error': 'Failed to process payment'}, status=500)
+            return Response({'error': 'Failed to reserve inventory'}, status=500)
 
-        return Response({'status': 'Purchase Completed'})
+        return Response({'status': 'Pedido finalizado'})
